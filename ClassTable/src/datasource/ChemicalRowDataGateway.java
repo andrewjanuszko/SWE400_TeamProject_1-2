39a5516d92afb50971693de8d3f763aff5411d2f
ko1568 <kim@157.160.47.228>
2020-09-18 15:08:12 -0400

    getters and setters for chemical

--- ClassTable/src/datasource/ChemicalRowDataGateway.java
+++ ClassTable/src/datasource/ChemicalRowDataGateway.java
@@ -1,15 +1,18 @@
 package ClassTable.src.datasource;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import datasource.DatabaseManager;
 
 public class ChemicalRowDataGateway {
 
   public void createTableChemcial() {
     String dropTable = "DROP TABLE IF EXISTS Chemical;";
-    String createTable = "CREATE TABLE Chemical" + "(" + "chemicalID INT NOT NULL, "
-        + "name VARCHAR(20), " + "inhabits VARCHAR(20), " + "PRIMARY KEY (chemicalID)"+ ");";
-    
+    String createTable = "CREATE TABLE Chemical" + "(" + "chemicalId INT NOT NULL, " + "name VARCHAR(20), "
+        + "inhabits VARCHAR(20), " + "PRIMARY KEY (chemicalId)" + ");";
+
     try {
       Statement statement = DatabaseManager.getSingleton().getConnection().createStatement();
       // Drop the table if exists first
@@ -20,8 +23,31 @@ public class ChemicalRowDataGateway {
       e.printStackTrace();
     }
   }
-  
-  
-  
+
+  public String getName(int chemicalId) {
+    String name; 
+    String sql = new String("SELECT * FROM Chemical WHERE chemicalId = " + chemicalId + ";");
+    try {
+      Statement statement = DatabaseManager.getSingleton().getConnection().createStatement();
+      ResultSet rs = statement.executeQuery(sql);
+      rs.next();
+      name = rs.getString("name");
+    } catch (Exception e) { 
+      
+    }
+  }
+
+  public String getInhabits(int chemicalId) {
+    String inhabits; 
+    String sql = new String("SELECT * FROM Chemical WHERE chemicalId = " + chemicalId + ";");
+    try {
+      Statement statement = DatabaseManager.getSingleton().getConnection().createStatement();
+      ResultSet rs = statement.executeQuery(sql);
+      rs.next();
+      inhabits = rs.getString("inhabits");
+    } catch (Exception e) { 
+      
+    }
+  }
   
 }
