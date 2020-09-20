f70e73636b775e9b972775e60f67c58ae053bc39
ko1568 <kim@157.160.47.228>
2020-09-19 20:49:17 -0400

    forgot to change my branch

--- ClassTable/src/datasource/AcidRowDataGateway.java
+++ ClassTable/src/datasource/AcidRowDataGateway.java
@@ -1,24 +1,8 @@
 package datasource;
 
-import java.sql.Statement;
-
-public class AcidRowDataGateway {
+public abstract class AcidRowDataGateway {
   
-  public void createTableBase() {
-    String dropTable = "DROP TABLE IF EXISTS Acid;";
-    String createTable = "CREATE TABLE Acid" + "(" + "acidId INT NOT NULL, "
-        + "solute VARCHAR(20), "   
-        + "FOREIGN KEY(acidId) REFERENCES Chemical(chemicalId)"
-        + ");";
-    try {
-      Statement statement = DatabaseManager.getSingleton().getConnection().createStatement();
-      // Drop the table if exists first
-      statement.executeUpdate(dropTable);
-      // Create new Monitorings Table
-      statement.executeUpdate(createTable);
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
+  public abstract void createTableAcid();
   
+  public abstract String getSolute();
 }
---
+++ ClassTable/src/datasource/AcidRowDataGatewayRDS.java
@@ -0,0 +1,28 @@
+package datasource;
+
+import java.sql.Statement;
+
+public class AcidRowDataGatewayRDS extends AcidRowDataGateway {
+
+  @Override
+  public void createTableAcid() {
+    String dropTable = "DROP TABLE IF EXISTS Acid;";
+    String createTable = "CREATE TABLE Acid" + "(" + "acidId INT NOT NULL, " + "solute VARCHAR(20), "
+        + "FOREIGN KEY(acidId) REFERENCES Chemical(chemicalId)" + ");";
+    try {
+      Statement statement = DatabaseManager.getSingleton().getConnection().createStatement();
+      // Drop the table if exists first
+      statement.executeUpdate(dropTable);
+      // Create new Monitorings Table
+      statement.executeUpdate(createTable);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public String getSolute() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+}
