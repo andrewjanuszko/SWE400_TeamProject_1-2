472c7c85f3a5cb79f13174b9f68cff7357177127
ko1568 <kim@157.160.47.228>
2020-09-20 11:47:20 -0400

    refactor part 2

--- ClassTable/src/datasource/BaseRowDataGatewayRDS.java
+++ ClassTable/src/datasource/BaseRowDataGatewayRDS.java
@@ -1,5 +1,25 @@
 package datasource;
 
-public class BaseRowDataGatewayRDS {
+import java.sql.Statement;
 
+public class BaseRowDataGatewayRDS implements BaseRowDataGateway {
+
+  public void createTableBase() {
+    String dropTable = "DROP TABLE IF EXISTS Base;";
+    String createTable = "CREATE TABLE Base" + "(" 
+        + "baseId INT NOT NULL, "
+        + "solute VARCHAR(20), " 
+        + "FOREIGN KEY(baseId) REFERENCES Chemical(chemicalId)"
+        + ");"; // Not sure how to store solute, is a chemical
+    
+    try {
+      Statement statement = DatabaseManager.getSingleton().getConnection().createStatement();
+      // Drop the table if exists first
+      statement.executeUpdate(dropTable);
+      // Create new Monitorings Table
+      statement.executeUpdate(createTable);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
