3b1ec49d95e59cfd9365e20591d925f092a9cb03
jg2446 <jg2446@cs.ship.edu>
2020-09-26 21:41:43 -0400

    Made some tests :)

---
+++ ConcreteTable/test/datasource/AcidRowDataGatewayRDSTest.java
@@ -0,0 +1,15 @@
+package datasource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AcidRowDataGatewayRDSTest {
+
+  @Test
+  void test() {
+    System.out.println("wow");
+    fail("Not yet implemented");
+  }
+
+}
---
+++ ConcreteTable/test/datasource/MetalRowDataGatewayRDSTest.java
@@ -0,0 +1,15 @@
+package datasource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MetalRowDataGatewayRDSTest {
+
+  @Test
+  void test() {
+    System.out.println("hey baby girl");
+    fail("Not yet implemented");
+  }
+
+}
