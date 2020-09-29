c09ee1838b09fe8dcd419c63357148fcd2ea87e7
CB5522 <cb5522@ship.edu>
2020-09-29 19:38:50 -0400

    testgetters

--- ConcreteTable/test/datasource/AcidRowDataGatewayRDSTest.java
+++ ConcreteTable/test/datasource/AcidRowDataGatewayRDSTest.java
@@ -5,11 +5,15 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class AcidRowDataGatewayRDSTest {
-
+	 
   @Test
-  void test() {
-    System.out.println("wow");
-    fail("Not yet implemented");
+  void testGetters() throws DatabaseException{
+	 AcidRowDataGateway acid1 = new AcidRowDataGatewayRDS(1, "acid", "inhabit", "solute");
+	 
+	 assertEquals(1, acid1.getAcidID());
+	 assertEquals("acid", acid1.getName());
+	 assertEquals("inhabit", acid1.getInhabits());
+	 assertEquals("solute", acid1.getSolute());
   }
 
 }
