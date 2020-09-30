90514fbc136708ce91939e7f6d9880989b1e9901
Isabella Boone <ib2573@cs.ship.edu>
2020-09-30 14:44:07 -0400

    Update TestAcid.java

--- ClassTable/test/datasource/TestAcid.java
+++ ClassTable/test/datasource/TestAcid.java
@@ -11,11 +11,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 /**
- * TODO: Test update to nonexistant id
- * TODO: Test delete
- * TODO: Test changing to invalid ids
  * @author Isabella
- *
  */
 class TestAcid extends DatabaseTest {
   
