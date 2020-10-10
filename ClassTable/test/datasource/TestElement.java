c3cf1540baf0f397a37f4a1ee0b5650ed79762e2
Isabella <ib2573@cs.ship.edu>
2020-10-10 18:37:40 -0400

    test element fixed

--- ClassTable/test/datasource/TestElement.java
+++ ClassTable/test/datasource/TestElement.java
@@ -12,46 +12,60 @@ import org.junit.jupiter.api.Test;
  */
 class TestElement extends DatabaseTest {
 
-  
   @Test
   static void testGetAtomicNumber() {    
-    //initialize element
-    ElementRowDataGateway element = new ElementRowDataGatewayRDS(1, 55, 123, "chemicalname1", "inhabits1");
-    
-    //element getter
-    ElementRowDataGateway elementGet = new ElementRowDataGatewayRDS(1);
-    
-    assertEquals(55, elementGet.getAtomicNumber());
+    ElementRowDataGateway elementGet1 = new ElementRowDataGatewayRDS(21);
+    ElementRowDataGateway elementGet2 = new ElementRowDataGatewayRDS(22);
+    ElementRowDataGateway elementGet3 = new ElementRowDataGatewayRDS(23);
+    ElementRowDataGateway elementGet4 = new ElementRowDataGatewayRDS(24);
+
+    assertEquals(1, elementGet1.getAtomicNumber());
+    assertEquals(2, elementGet2.getAtomicNumber());
+    assertEquals(3, elementGet3.getAtomicNumber());
+    assertEquals(4, elementGet4.getAtomicNumber());
   }
   
   @Test
   static void testGetAtomicMass() {    
-    ElementRowDataGateway element = new ElementRowDataGatewayRDS(1, 55, 123, "chemicalname1", "inhabits1");
-    
-    ElementRowDataGateway elementGet = new ElementRowDataGatewayRDS(1);
-    
-    assertEquals(123, elementGet.getAtomicMass(), .01);
+    ElementRowDataGateway elementGet1 = new ElementRowDataGatewayRDS(21);
+    ElementRowDataGateway elementGet2 = new ElementRowDataGatewayRDS(22);
+    ElementRowDataGateway elementGet3 = new ElementRowDataGatewayRDS(23);
+    ElementRowDataGateway elementGet4 = new ElementRowDataGatewayRDS(24);
+
+    assertEquals(9, elementGet1.getAtomicMass(), 0.1);
+    assertEquals(8, elementGet2.getAtomicMass(), 0.1);
+    assertEquals(7, elementGet3.getAtomicMass(), 0.1);
+    assertEquals(6, elementGet4.getAtomicMass(), 0.1);
   }
   
   @Test
   static void testGetName() {    
-    ElementRowDataGateway element = new ElementRowDataGatewayRDS(1, 55, 123, "chemicalname1", "inhabits1");
-    
-    ElementRowDataGateway elementGet = new ElementRowDataGatewayRDS(1);
-    
-    assertEquals("chemicalname1", elementGet.getName());
+    ElementRowDataGateway elementGet1 = new ElementRowDataGatewayRDS(21);
+    ElementRowDataGateway elementGet2 = new ElementRowDataGatewayRDS(22);
+    ElementRowDataGateway elementGet3 = new ElementRowDataGatewayRDS(23);
+    ElementRowDataGateway elementGet4 = new ElementRowDataGatewayRDS(24);
+
+    assertEquals("elementname1", elementGet1.getName());
+    assertEquals("elementname2", elementGet2.getName());
+    assertEquals("elementname3", elementGet3.getName());
+    assertEquals("elementname4", elementGet4.getName());
   }
   
   @Test
   static void testGetInhabits() {    
-    ElementRowDataGateway element = new ElementRowDataGatewayRDS(1, 15, 18, "chemicalname1", "inhabits1");
-    
-    ElementRowDataGateway elementGet = new ElementRowDataGatewayRDS(1);
-    
-    assertEquals("inhabits1", elementGet.getInhabits());
+    ElementRowDataGateway elementGet1 = new ElementRowDataGatewayRDS(21);
+    ElementRowDataGateway elementGet2 = new ElementRowDataGatewayRDS(22);
+    ElementRowDataGateway elementGet3 = new ElementRowDataGatewayRDS(23);
+    ElementRowDataGateway elementGet4 = new ElementRowDataGatewayRDS(24);
+
+    assertEquals("elementinhabits1", elementGet1.getInhabits());
+    assertEquals("elementinhabits2", elementGet2.getInhabits());
+    assertEquals("elementinhabits3", elementGet3.getInhabits());
+    assertEquals("elementinhabits4", elementGet4.getInhabits());
   }
   
   static void testAll() {
+    insertElements(); 
     testGetAtomicNumber();
     testGetAtomicMass();
     testGetName();
@@ -59,7 +73,10 @@ class TestElement extends DatabaseTest {
   }
   
   private static void insertElements() {
-    
+    ElementRowDataGateway ele = new ElementRowDataGatewayRDS(21, 1, 9, "elementname1", "elementinhabits1");
+    ele = new ElementRowDataGatewayRDS(22, 2, 8, "elementname2", "elementinhabits2");
+    ele = new ElementRowDataGatewayRDS(23, 3, 7, "elementname3", "elementinhabits3");
+    ele = new ElementRowDataGatewayRDS(24, 4, 6, "elementname4", "elementinhabits4");
   }
 
 }
