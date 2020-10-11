3683e1a39a7edbb6a34dfb525d03b412042f29af
Andrew Januszko <58179101+andrewjanuszko@users.noreply.github.com>
2020-10-10 23:31:19 -0400

    Updated ChemicalRDS to hold inventory and moles.

--- SingleTable/src/datasource/ChemicalRowDataGateway.java
+++ SingleTable/src/datasource/ChemicalRowDataGateway.java
@@ -2,110 +2,142 @@ package datasource;
 
 /**
  * Interface for a ChemicalRowDataGateway
+ * 
  * @author andrewjanuszko
  */
 public interface ChemicalRowDataGateway {
-	
-	/**
-	 * Deletes an instance of a Chemical from the table.
-	 * @throws DatabaseException when things go wrong.
-	 */
-	public void delete() throws DatabaseException;
-	
-	/**
-	 * Updates the Chemical in the table.
-	 * @throws DatabaseException when things go wrong.
-	 */
-	public void update() throws DatabaseException;
-	
-	/**
-	 * Get the ID of the Chemical.
-	 * @return the Chemical ID.
-	 */
-	public int getChemicalID();
-	
-	/**
-	 * Get the type of the Chemical.
-	 * @return the type.
-	 */
-	public int getType();
-	
-	/**
-	 * Get the name of the Chemical.
-	 * @return the name.
-	 */
-	public String getName();
-	
-	/**
-	 * Get the habitat of the Chemical.
-	 * @return the habitat.
-	 */
-	public String getInhabits();
-	
-	/**
-	 * Get the atomic number of the Chemical.
-	 * @return the atomic number.
-	 */
-	public int getAtomicNumber();
-	
-	/**
-	 * Get the atomic mass of the Chemical.
-	 * @return the atomic mass.
-	 */
-	public double getAtomicMass();
-	
-	/**
-	 * Get the Acid that a Metal is dissolved by.
-	 * @return the ID of the Acid.
-	 */
-	public int getDissolvedBy();
-	
-	/**
-	 * Get the Chemical ID of the solute.
-	 * @return the Chemical ID.
-	 */
-	public int getSolute();
-	
-	/**
-	 * Set the type of the Chemical.
-	 * @param type the type of the Chemical.
-	 */
-	public void setType(int type);
-	
-	/**
-	 * Set the name of the Chemical.
-	 * @param name the name of the Chemical.
-	 */
-	public void setName(String name);
-	
-	/**
-	 * Set the habitat of the chemical.
-	 * @param inhabits is the habitat of the chemical.
-	 */
-	public void setInhabits(String inhabits);
-	
-	/**
-	 * Set the atomic number of the Chemical.
-	 * @param atomicNumber the atomic number of the Chemical.
-	 */
-	public void setAtomicNumber(int atomicNumber);
-	
-	/**
-	 * Set the atomic mass of the Chemical.
-	 * @param atomicMass the atomic mass of the Chemical.
-	 */
-	public void setAtomicMass(double atomicMass);
 
-	/**
-	 * Set the Acid ID that a Metal is dissolved by.
-	 * @param dissolvedBy is the Acid ID that a Metal is dissolved by.
-	 */
-	public void setDissolvedBy(int dissolvedBy);
-	
-	/**
-	 * Set the Chemical ID of the solute.
-	 * @param solute the Chemical ID of the solute.
-	 */
-	public void setSolute(int solute);
-	
+  /**
+   * Deletes an instance of a Chemical from the table.
+   * 
+   * @throws DatabaseException when things go wrong.
+   */
+  public void delete() throws DatabaseException;
+
+  /**
+   * Updates the Chemical in the table.
+   * 
+   * @throws DatabaseException when things go wrong.
+   */
+  public void update() throws DatabaseException;
+
+  /**
+   * Get the ID of the Chemical.
+   * 
+   * @return the Chemical ID.
+   */
+  public int getChemicalID();
+
+  /**
+   * Get the type of the Chemical.
+   * 
+   * @return the type.
+   */
+  public int getType();
+
+  /**
+   * Get the name of the Chemical.
+   * 
+   * @return the name.
+   */
+  public String getName();
+
+  /**
+   * Get the habitat of the Chemical.
+   * 
+   * @return the habitat.
+   */
+  public double getInventory();
+
+  /**
+   * Get the atomic number of the Chemical.
+   * 
+   * @return the atomic number.
+   */
+  public int getAtomicNumber();
+
+  /**
+   * Get the atomic mass of the Chemical.
+   * 
+   * @return the atomic mass.
+   */
+  public double getAtomicMass();
+
+  /**
+   * Get the Acid that a Metal is dissolved by.
+   * 
+   * @return the ID of the Acid.
+   */
+  public int getDissolvedBy();
+
+  /**
+   * Get the amount of acid moles required to dissolve a metal.
+   * 
+   * @return the amount of acid moles required to dissolve a metal.
+   */
+  public double getMoles();
+
+  /**
+   * Get the Chemical ID of the solute.
+   * 
+   * @return the Chemical ID.
+   */
+  public int getSolute();
+
+  /**
+   * Set the type of the Chemical.
+   * 
+   * @param type the type of the Chemical.
+   */
+  public void setType(int type);
+
+  /**
+   * Set the name of the Chemical.
+   * 
+   * @param name the name of the Chemical.
+   */
+  public void setName(String name);
+
+  /**
+   * Set the habitat of the chemical.
+   * 
+   * @param inhabits is the habitat of the chemical.
+   */
+  public void setInventory(double inventory);
+
+  /**
+   * Set the atomic number of the Chemical.
+   * 
+   * @param atomicNumber the atomic number of the Chemical.
+   */
+  public void setAtomicNumber(int atomicNumber);
+
+  /**
+   * Set the atomic mass of the Chemical.
+   * 
+   * @param atomicMass the atomic mass of the Chemical.
+   */
+  public void setAtomicMass(double atomicMass);
+
+  /**
+   * Set the Acid ID that a Metal is dissolved by.
+   * 
+   * @param dissolvedBy is the Acid ID that a Metal is dissolved by.
+   */
+  public void setDissolvedBy(int dissolvedBy);
+
+  /**
+   * Set the amount of acid moles required to dissolve a metal.
+   * 
+   * @param moles the amount of acid moles required to dissolve a metal.
+   */
+  public void setMoles(double moles);
+
+  /**
+   * Set the Chemical ID of the solute.
+   * 
+   * @param solute the Chemical ID of the solute.
+   */
+  public void setSolute(int solute);
+
 }
--- SingleTable/test/datasource/ChemicalRDGRDSTest.java
+++ SingleTable/test/datasource/ChemicalRDGRDSTest.java
@@ -1,166 +1,184 @@
 package datasource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
 
 public class ChemicalRDGRDSTest extends DatabaseTest {
-	
-	/**
-	 * Test the creation of a table and inserting into it.
-	 * @throws DatabaseException when things go wrong.
-	 */
-	@Test
-	public void testCreateAndInsert() throws DatabaseException {
-		ChemicalRowDataGatewayRDS.createTable();
-		
-		ChemicalRowDataGatewayRDS unknownChemical = new ChemicalRowDataGatewayRDS(0, "UnknownChemical", "The Moon", 0, 0, 0, 0);
-		
-		assertEquals(1, unknownChemical.getChemicalID());
-		assertEquals(0, unknownChemical.getType());
-		assertEquals("UnknownChemical", unknownChemical.getName());
-		assertEquals("The Moon", unknownChemical.getInhabits());
-		assertEquals(0, unknownChemical.getAtomicNumber());
-		assertEquals(0, unknownChemical.getAtomicMass(), 0.001);
-		assertEquals(0, unknownChemical.getDissolvedBy());
-		assertEquals(0, unknownChemical.getSolute());
-		
-		ChemicalRowDataGatewayRDS funkyChemical = new ChemicalRowDataGatewayRDS(1, "FunkyChemical", "Mars", 17, 9.810, 0, 0);
-		
-		assertEquals(2, funkyChemical.getChemicalID());
-		assertEquals(1, funkyChemical.getType());
-		assertEquals("FunkyChemical", funkyChemical.getName());
-		assertEquals("Mars", funkyChemical.getInhabits());
-		assertEquals(17, funkyChemical.getAtomicNumber());
-		assertEquals(9.810, funkyChemical.getAtomicMass(), 0.001);
-		assertEquals(0, funkyChemical.getDissolvedBy());
-		assertEquals(0, funkyChemical.getSolute());
-		
-		ChemicalRowDataGatewayRDS.dropTable();
-	}
-	
-	/**
-	 * Test the insertion of duplicate entries.
-	 * @throws DatabaseException when things go wrong.
-	 */
-	@Test
-	public void testDuplicateEntry() throws DatabaseException {
-		ChemicalRowDataGatewayRDS.createTable();
-		
-		ChemicalRowDataGatewayRDS unknownChemical = new ChemicalRowDataGatewayRDS(0, "UnknownChemical", "The Moon", 0, 0, 0, 0);
-		
-		assertEquals(1, unknownChemical.getChemicalID());
-		assertEquals(0, unknownChemical.getType());
-		assertEquals("UnknownChemical", unknownChemical.getName());
-		assertEquals("The Moon", unknownChemical.getInhabits());
-		assertEquals(0, unknownChemical.getAtomicNumber());
-		assertEquals(0, unknownChemical.getAtomicMass(), 0.001);
-		assertEquals(0, unknownChemical.getDissolvedBy());
-		assertEquals(0, unknownChemical.getSolute());
-		
-		try {
-			ChemicalRowDataGatewayRDS unknownChemical2 = new ChemicalRowDataGatewayRDS(0, "UnknownChemical", "The Moon", 0, 0, 0, 0);
-			assertEquals("UnknownChemical", unknownChemical2.getName());
-			fail();
-		} catch(DatabaseException e) {
-			assertTrue(true);
-			ChemicalRowDataGatewayRDS.dropTable();
-		}	
-	}
-	
-	/**
-	 * Test if drop table works.
-	 * @throws DatabaseException when things go wrong.
-	 */
-	@Test
-	public void testDropTable() throws DatabaseException {
-		ChemicalRowDataGatewayRDS.createTable();
-		
-		ChemicalRowDataGatewayRDS carbon = new ChemicalRowDataGatewayRDS(1, "Carbon", "Earth", 6, 12.011, 0, 0);
-		
-		assertEquals(1, carbon.getChemicalID());
-		assertEquals(1, carbon.getType());
-		assertEquals("Carbon", carbon.getName());
-		assertEquals(6, carbon.getAtomicNumber());
-		assertEquals(12.011, carbon.getAtomicMass(), 0.001);
-		assertEquals(0, carbon.getDissolvedBy());
-		assertEquals(0, carbon.getSolute());
-		
-		ChemicalRowDataGatewayRDS.dropTable();
 
-		try {
-			ChemicalRowDataGatewayRDS funkyChemical = new ChemicalRowDataGatewayRDS(1, "FunkyChemical", "Mars", 17, 9.810, 0, 0);
-			assertEquals("FunkyChemical", funkyChemical.getName());
-			fail();
-		} catch(DatabaseException e) {
-			assertTrue(true);
-		}					
-	}
-	
-	/**
-	 * Test dropping a chemical from the table.
-	 * @throws DatabaseException when things go wrong.
-	 */
-	@Test
-	public void testDeleteChemical() throws DatabaseException {
-		ChemicalRowDataGatewayRDS.createTable();
-		
-		ChemicalRowDataGatewayRDS unknownChemical = new ChemicalRowDataGatewayRDS(0, "UnknownChemical", "The Moon", 0, 0, 0, 0);
-		
-		assertEquals(1, unknownChemical.getChemicalID());
-		assertEquals(0, unknownChemical.getType());
-		assertEquals("UnknownChemical", unknownChemical.getName());
-		assertEquals("The Moon", unknownChemical.getInhabits());
-		assertEquals(0, unknownChemical.getAtomicNumber());
-		assertEquals(0, unknownChemical.getAtomicMass(), 0.001);
-		assertEquals(0, unknownChemical.getDissolvedBy());
-		assertEquals(0, unknownChemical.getSolute());
-		
-		unknownChemical.delete();
-		
-		try {
-			ChemicalRowDataGatewayRDS unknownChemicalSecond = new ChemicalRowDataGatewayRDS(1);
-			assertEquals("UnknownChemical", unknownChemicalSecond.getName());
-			fail();
-		} catch(DatabaseException e) {
-			assertTrue(true);
-			ChemicalRowDataGatewayRDS.dropTable();
-		}
-	}
-	
-	/**
-	 * Test updating an entry in the database.
-	 * @throws DatabaseException when things go wrong.
-	 */
-	@Test
-	public void testUpdate() throws DatabaseException {
-		ChemicalRowDataGatewayRDS.createTable();
-		
-		ChemicalRowDataGatewayRDS carbon = new ChemicalRowDataGatewayRDS(1, "Carbon", "Earth", 6, 12.011, 0, 0);
-		
-		assertEquals(1, carbon.getChemicalID());
-		assertEquals(1, carbon.getType());
-		assertEquals("Carbon", carbon.getName());
-		assertEquals(6, carbon.getAtomicNumber());
-		assertEquals(12.011, carbon.getAtomicMass(), 0.001);
-		assertEquals(0, carbon.getDissolvedBy());
-		assertEquals(0, carbon.getSolute());
-		
-		carbon.setName("Carbon-13");
-		carbon.setAtomicMass(13.003);
-		carbon.update();
-		
-		ChemicalRowDataGatewayRDS carbon13 = new ChemicalRowDataGatewayRDS(1);
-		
-		assertEquals(1, carbon13.getChemicalID());
-		assertEquals(1, carbon13.getType());
-		assertEquals("Carbon-13", carbon13.getName());
-		assertEquals(13.003, carbon13.getAtomicMass(), 0.001);
-		assertEquals(0, carbon13.getDissolvedBy());
-		assertEquals(0, carbon13.getSolute());
-		
-		ChemicalRowDataGatewayRDS.dropTable();
-	}
+  /**
+   * Test the creation of a table and inserting into it.
+   * 
+   * @throws DatabaseException when things go wrong.
+   */
+  @Test
+  public void testCreateAndInsert() throws DatabaseException {
+    ChemicalRowDataGatewayRDS.createTable();
+
+    ChemicalRowDataGatewayRDS unknown = new ChemicalRowDataGatewayRDS(0, "Unknown", 123.0, 0, 0, 0, 0, 0);
+
+    assertEquals(1, unknown.getChemicalID());
+    assertEquals(0, unknown.getType());
+    assertEquals("Unknown", unknown.getName());
+    assertEquals(123.0, unknown.getInventory(), 0.001);
+    assertEquals(0, unknown.getAtomicNumber());
+    assertEquals(0, unknown.getAtomicMass(), 0.001);
+    assertEquals(0, unknown.getDissolvedBy());
+    assertEquals(0, unknown.getMoles(), 0.001);
+    assertEquals(0, unknown.getSolute());
+
+    ChemicalRowDataGatewayRDS funky = new ChemicalRowDataGatewayRDS(1, "Funky", 9.0, 17, 9.810, 0, 0, 0);
+
+    assertEquals(2, funky.getChemicalID());
+    assertEquals(1, funky.getType());
+    assertEquals("Funky", funky.getName());
+    assertEquals(9.0, funky.getInventory());
+    assertEquals(17, funky.getAtomicNumber());
+    assertEquals(9.810, funky.getAtomicMass(), 0.001);
+    assertEquals(0, funky.getDissolvedBy());
+    assertEquals(0, funky.getMoles(), 0.001);
+    assertEquals(0, funky.getSolute());
+
+    ChemicalRowDataGatewayRDS.dropTable();
+  }
+
+  /**
+   * Test the insertion of duplicate entries.
+   * 
+   * @throws DatabaseException when things go wrong.
+   */
+  @Test
+  public void testDuplicateEntry() throws DatabaseException {
+    ChemicalRowDataGatewayRDS.createTable();
+
+    ChemicalRowDataGatewayRDS unknown = new ChemicalRowDataGatewayRDS(0, "Unknown", 123.0, 0, 0, 0, 0, 0);
+
+    assertEquals(1, unknown.getChemicalID());
+    assertEquals(0, unknown.getType());
+    assertEquals("Unknown", unknown.getName());
+    assertEquals(123.0, unknown.getInventory(), 0.001);
+    assertEquals(0, unknown.getAtomicNumber());
+    assertEquals(0, unknown.getAtomicMass(), 0.001);
+    assertEquals(0, unknown.getDissolvedBy());
+    assertEquals(0, unknown.getMoles(), 0.001);
+    assertEquals(0, unknown.getSolute());
+
+    try {
+      @SuppressWarnings("unused")
+      ChemicalRowDataGatewayRDS unknown2 = new ChemicalRowDataGatewayRDS(0, "Unknown", 123.0, 0, 0, 0, 0, 0);
+      fail();
+    } catch (DatabaseException e) {
+      assertEquals("Failed to insert into 'Chemical' table.", e.getSimpleDescription());
+      ChemicalRowDataGatewayRDS.dropTable();
+    }
+  }
+
+  /**
+   * Test if drop table works.
+   * 
+   * @throws DatabaseException when things go wrong.
+   */
+  @Test
+  public void testDropTable() throws DatabaseException {
+    ChemicalRowDataGatewayRDS.createTable();
+
+    ChemicalRowDataGatewayRDS carbon = new ChemicalRowDataGatewayRDS(1, "Carbon", 1.0, 6, 12.011, 0, 0, 0);
+
+    assertEquals(1, carbon.getChemicalID());
+    assertEquals(1, carbon.getType());
+    assertEquals("Carbon", carbon.getName());
+    assertEquals(1, carbon.getInventory(), 0.001);
+    assertEquals(6, carbon.getAtomicNumber());
+    assertEquals(12.011, carbon.getAtomicMass(), 0.001);
+    assertEquals(0, carbon.getDissolvedBy());
+    assertEquals(0, carbon.getMoles(), 0.001);
+    assertEquals(0, carbon.getSolute());
+
+    ChemicalRowDataGatewayRDS.dropTable();
+
+    try {
+      @SuppressWarnings("unused")
+      ChemicalRowDataGatewayRDS funky = new ChemicalRowDataGatewayRDS(1, "Funky", 9.0, 17, 9.810, 0, 0, 0);
+      fail();
+    } catch (DatabaseException e) {
+      assertEquals("Failed to insert into 'Chemical' table.", e.getSimpleDescription());
+    }
+  }
+
+  /**
+   * Test dropping a chemical from the table.
+   * 
+   * @throws DatabaseException when things go wrong.
+   */
+  @Test
+  public void testDeleteChemical() throws DatabaseException {
+
+    ChemicalRowDataGatewayRDS.dropTable();
+    ChemicalRowDataGatewayRDS.createTable();
+
+    ChemicalRowDataGatewayRDS unknown = new ChemicalRowDataGatewayRDS(0, "Unknown", 123.0, 0, 0, 0, 0, 0);
+
+    assertEquals(1, unknown.getChemicalID());
+    assertEquals(0, unknown.getType());
+    assertEquals("Unknown", unknown.getName());
+    assertEquals(123.0, unknown.getInventory(), 0.001);
+    assertEquals(0, unknown.getAtomicNumber());
+    assertEquals(0, unknown.getAtomicMass(), 0.001);
+    assertEquals(0, unknown.getDissolvedBy());
+    assertEquals(0, unknown.getMoles(), 0.001);
+    assertEquals(0, unknown.getSolute());
+
+    unknown.delete();
+
+    try {
+      @SuppressWarnings("unused")
+      ChemicalRowDataGatewayRDS unknown2 = new ChemicalRowDataGatewayRDS(1);
+      fail();
+    } catch (DatabaseException e) {
+      assertEquals("Could not find Chemical with ID 1.", e.getSimpleDescription());
+      ChemicalRowDataGatewayRDS.dropTable();
+    }
+  }
+
+  /**
+   * Test updating an entry in the database.
+   * 
+   * @throws DatabaseException when things go wrong.
+   */
+  @Test
+  public void testUpdate() throws DatabaseException {
+    ChemicalRowDataGatewayRDS.createTable();
+
+    ChemicalRowDataGatewayRDS carbon = new ChemicalRowDataGatewayRDS(1, "Carbon", 1.0, 6, 12.011, 0, 0, 0);
+
+    assertEquals(1, carbon.getChemicalID());
+    assertEquals(1, carbon.getType());
+    assertEquals("Carbon", carbon.getName());
+    assertEquals(1, carbon.getInventory(), 0.001);
+    assertEquals(6, carbon.getAtomicNumber());
+    assertEquals(12.011, carbon.getAtomicMass(), 0.001);
+    assertEquals(0, carbon.getDissolvedBy());
+    assertEquals(0, carbon.getMoles(), 0.001);
+    assertEquals(0, carbon.getSolute());
+
+    carbon.setName("Carbon-13");
+    carbon.setInventory(12.345);
+    carbon.setAtomicMass(13.003);
+    carbon.update();
+
+    ChemicalRowDataGatewayRDS carbon13 = new ChemicalRowDataGatewayRDS(1);
+
+    assertEquals(1, carbon.getChemicalID());
+    assertEquals(1, carbon.getType());
+    assertEquals("Carbon-13", carbon13.getName());
+    assertEquals(12.345, carbon.getInventory(), 0.001);
+    assertEquals(6, carbon.getAtomicNumber());
+    assertEquals(13.003, carbon13.getAtomicMass(), 0.001);
+    assertEquals(0, carbon.getDissolvedBy());
+    assertEquals(0, carbon.getMoles(), 0.001);
+    assertEquals(0, carbon.getSolute());
+
+    ChemicalRowDataGatewayRDS.dropTable();
+  }
 
 }
