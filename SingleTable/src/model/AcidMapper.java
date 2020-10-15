0a9ee845ebe091561325ed54b52487e13dd196f1
MorganWB <mw2181@cs.ship.edu>
2020-10-15 18:04:16 -0400

    AcidMapper Started

---
+++ SingleTable/src/model/AcidMapper.java
@@ -0,0 +1,61 @@
+package model;
+
+import java.util.ArrayList;
+
+public class AcidMapper implements AcidDataMapperInterface  {
+
+	@Override
+	public void create(Acid acid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Acid read(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Acid acid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Acid acid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<Acid> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Acid> filterByWildCardName(String wildCardName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Acid> filterByInventory(double inventory) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Acid> filterByInventoryRange(double min, double max) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Acid> filterBySolute(int chemicalID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
