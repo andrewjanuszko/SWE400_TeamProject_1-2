42bd3f0837696a1a63a593f715423a7018bc959b
ko1568 <kim@157.160.47.228>
2020-10-17 23:17:21 -0400

    exceptions in commands

--- Project2/src/model/CreateNewElementCommand.java
+++ Project2/src/model/CreateNewElementCommand.java
@@ -5,18 +5,30 @@ import mappers.ElementDataMapper;
 public class CreateNewElementCommand implements Command {
 
   Element element;
+
   public CreateNewElementCommand(Element element) {
     this.element = element;
   }
+
   @Override
   public void execute() {
     ElementDataMapperInterface elementMapper = new ElementDataMapper();
     try {
-      elementMapper.create(element.getName(), element.getInventory(), element.getAtomicNumber(), element.getAtomicMass());
-    } catch (DomainModelException e) {
+      if (element.getAtomicMass() < element.getAtomicNumber()) {
+        throw new Exception("Atomic Number cannot be more than Atomic Mass");
+      } else if (element.getName().contains(" ")) {
+        throw new Exception("Element Names cannot contain a space");
+      } else {
+        elementMapper.create(element.getName(), element.getInventory(), element.getAtomicNumber(),
+            element.getAtomicMass());
+      }
+    } catch (
+
+    DomainModelException e) {
+      e.printStackTrace();
+    } catch (Exception e) {
       e.printStackTrace();
     }
-    
   }
 
 }
--- Project2/src/model/CreateNewMetalCommand.java
+++ Project2/src/model/CreateNewMetalCommand.java
@@ -14,12 +14,18 @@ public class CreateNewMetalCommand implements Command {
   public void execute() {
     MetalDataMapperInterface metalMapper = new MetalDataMapper();
     try {
-      metalMapper.create(metal.getName(), metal.getInventory(), metal.getAtomicNumber(), metal.getAtomicMass(),
-          metal.getAcidAmount());
+      if(metal.getAtomicNumber() > metal.getAtomicMass()) {
+        throw new Exception("Atomic Number cannot be more than Atomic Mass");
+      } else {
+      
+        metalMapper.create(metal.getName(), metal.getInventory(), metal.getAtomicNumber(), metal.getAtomicMass(),
+            metal.getAcidAmount());
+      }
     } catch (DomainModelException e) {
       e.printStackTrace();
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-
   }
 
 }
