d9bb9b4d4ac9893080b5ed6978dcc5e49007f9a2
jg2446 <jg2446@cs.ship.edu>
2020-10-22 19:19:25 -0400

    Fixed tests to reflect prior changes

--- ConcreteTable/test/datasource/CompoundMadeOfTableDataGatewayRDSTest.java
+++ ConcreteTable/test/datasource/CompoundMadeOfTableDataGatewayRDSTest.java
@@ -6,7 +6,8 @@ import org.junit.jupiter.api.Test;
 
 class CompoundMadeOfTableDataGatewayRDSTest extends DatabaseTest{
 
-	@Test
+	@SuppressWarnings("unused")
+  @Test
 	void testAddRelation() throws DatabaseException {
 		CompoundRowDataGateway c1 = new CompoundRowDataGatewayRDS(12, "compound", 1.0);
 		ElementRowDataGateway e1 = new ElementRowDataGatewayRDS(10, "element", 1.0, 5, 10.0);
