beb4d36f153f8fb043689f1d0e5241a207db63b7
Isabella Boone <ib2573@cs.ship.edu>
2020-10-21 20:19:33 -0400

    Revert "Merge branch 'master' into '58-6-2-add-modify-button-command'"
    This reverts merge request !95

--- Project2/src/presentation/AcidPanel.java
+++ Project2/src/presentation/AcidPanel.java
@@ -1,6 +1,8 @@
 package presentation;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -11,8 +13,8 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.util.ArrayList;
 import java.util.List;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -20,174 +22,13 @@ import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingConstants;
 
+import mappers.AcidDataMapper;
 import model.Acid;
 
-//import model.AcidDataMapperInterface;
-import model.DomainModelException;
+public class AcidPanel extends JPanel {
+  public AcidPanel() {
 
-public class AcidPanel extends JPanel{
 
-	JScrollPane acids = new JScrollPane(); 
-	GridBagConstraints gbc = new GridBagConstraints(); 
-	JButton addButton = new JButton("Add");
-	JButton deleteButton = new JButton("Delete");
-	JButton filterButton = new JButton("Filter");
-	JButton detailsButton = new JButton("Details");
-	JLabel selected = null;
-	int selectedID;
-	Color labelColor = new Color(30,30,30);
-	List<Acid> acidList;
-	//AcidDataMapper acidMapper = new AcidDataMapper();
-	String filter;
-	
-	public AcidPanel() {
-		this.setLayout(new GridBagLayout());
-		addScrollPane();
-		setButtons();
-	}
-
-	private void addScrollPane() {
-		acids.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		acids.add(acids.createVerticalScrollBar());
-		
-		acids.setViewportView(label());
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		gbc.weightx = 1;
-		gbc.weighty = Integer.MAX_VALUE;
-		gbc.fill = GridBagConstraints.BOTH;
-		add(acids,gbc);
-	}
-	
-	private JLabel label(){
-	    JLabel label = new JLabel("", SwingConstants.CENTER);
-	    label.setBackground(Color.WHITE);
-	    label.setOpaque(true);
-	    label.setPreferredSize(new Dimension(100,20));
-	    return label;
-	  }
-
-	
-	private void setButtons() {
-		addButton.addActionListener( new ActionListener() {
-		      @Override
-		      public void actionPerformed(ActionEvent ae) {
-		        addAcid();
-		      }
-		    });
-		deleteButton.addActionListener( new ActionListener() {
-		      @Override
-		      public void actionPerformed(ActionEvent ae) {
-		        deleteAcid();
-		      }
-		    });
-		filterButton.addActionListener( new ActionListener() {
-		      @Override
-		      public void actionPerformed(ActionEvent ae) {
-		        filterAcid();
-		      }
-		    });
-		detailsButton.addActionListener( new ActionListener() {
-		      @Override
-		      public void actionPerformed(ActionEvent ae) {
-		        getDetailsAcid();
-		      }
-		    });
-		JPanel buttons = new JPanel(new GridBagLayout());
-		gbc.fill = GridBagConstraints.HORIZONTAL;
-	    gbc.weightx = 0;
-	    gbc.gridx = 0;
-	    gbc.gridy = 0;
-	    buttons.add(addButton, gbc);
-	    
-	    gbc.gridx = 1;
-	    buttons.add(deleteButton, gbc);
-	    
-	    gbc.gridx = 2;
-	    buttons.add(filterButton, gbc);
-	    
-	    gbc.gridx = 3;
-	    buttons.add(detailsButton, gbc);
-	    
-	    gbc.gridx = 0;
-	    gbc.gridy = 1;
-	    gbc.anchor = GridBagConstraints.SOUTHWEST;
-	    gbc.weighty = 1;
-	    buttons.setBackground(Color.GRAY);
-	    
-	    add(buttons, gbc);
-
-	}
-	
-	private void addAcid() {
-		new AddAcidFrame().addWindowListener(new WindowAdapter() {
-			@Override
-			public void windowClosed(WindowEvent arg0) {
-				acids.setViewportView(label());
-			}
-		});
-	}
-	
-	private void deleteAcid() {
-		if(selected != null) {
-		}
-	}
-	
-	private void filterAcid() {
-			
-			FilterAcidFrame faf = new FilterAcidFrame();
-			    faf.addWindowListener(new WindowAdapter() {
-				@Override
-				public void windowClosed(WindowEvent arg0) {
-					filter = faf.getFilter();
-					System.out.println(filter);
-				}
-			});
-	}
-	
-	private void getDetailsAcid() {
-		if(selected == null) {
-			
-		}
-	}
-	
-	private void removeSelectedBackground() {
-	    if(selected != null)
-	      selected.setBackground(labelColor);
-	  }
-	/*
-	private JPanel buildLabels() {
-		JPanel labels = new JPanel();
-		try {
-			//acidList = acidMapper.getAll();
-		} catch (DomainModelException e) {
-			e.printStackTrace();
-		}
-
-		labels.setLayout(new GridLayout(acidList.size(), 1));
-		
-		for(int i = 0; i < acidList.size(); i++) {
-		      final int x = i;
-		      JLabel label = new JLabel(buildHtml(acidList.get(i)));
-		      label.setOpaque(true);
-		      label.setBackground(new Color(30, 30, 30));
-		      label.addMouseListener( new MouseAdapter() {
-		          @Override
-		          public void mouseClicked(MouseEvent e) {
-		              removeSelectedBackground();
-		              label.setBackground(new Color(234, 201, 55));
-		              selected = label;
-		              selectedID = acidList.get(x).getID();
-		          }
-		      }); 
-		      labels.add(label);
-		    }
-
-		return labels;
-	}
-    */
-	private String buildHtml(Acid acid) {
-		return acid.getName();
-	}
+  }
 
 }
